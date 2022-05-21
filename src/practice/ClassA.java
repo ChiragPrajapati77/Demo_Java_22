cd29ec9a7c92728f3453ffeb43070f3ea424609a
Chirag Prajapati <chirag.68.cp@gmail.com>
2022-05-21 15:14:24 +0530

    Changes in Branch B

--- src/practice/ClassA.java
+++ src/practice/ClassA.java
@@ -3,7 +3,8 @@ package practice;
 public class ClassA {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello Chirag");
+		System.out.println("Hello Chirag123");
+		System.out.println("changes in line 7");
 	}
 
 }
